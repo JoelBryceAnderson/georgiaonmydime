d7f6e4b082188ff030059b4059c3fd32d9dd5009
Joel Anderson <joelbryceanderson@gmail.com>
2018-06-17 19:38:00 -0400

    made bookmark icon stateful

--- lib/widgets/happyhour/BookmarkedIcon.dart
+++ lib/widgets/happyhour/BookmarkedIcon.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:georgiaonmydime/data/HappyHour.dart';
 
-class BookmarkedIcon extends StatelessWidget {
+class BookmarkedIcon extends StatefulWidget {
+  final HappyHour happyHour;
+
   const BookmarkedIcon({Key key, this.happyHour}) : super(key: key);
 
-  final HappyHour happyHour;
+  @override
+  State<StatefulWidget> createState() => new _BookmarkState(happyHour);
+}
+
+class _BookmarkState extends State<BookmarkedIcon> {
+
+  final HappyHour _happyHour;
+
+  _BookmarkState(this._happyHour);
 
   @override
   Widget build(BuildContext context) {
     return new IconButton(
-        icon: new Icon(happyHour.saved ? Icons.bookmark : Icons.bookmark_border,
+        icon: new Icon(_happyHour.saved ? Icons.bookmark : Icons.bookmark_border,
             color: Colors.blueAccent),
         onPressed: _onClicked);
   }
 
+
   void _onClicked() {
-    happyHour.saved = !happyHour.saved;
+    setState(() {
+      _happyHour.saved = !_happyHour.saved;
+    });
   }
+
 }
