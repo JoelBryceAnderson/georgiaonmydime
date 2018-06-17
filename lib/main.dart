7d96ef9f95ad763c2c99e55a008d349d4430c325
Joel Anderson <joelbryceanderson@gmail.com>
2018-06-17 11:54:11 -0400

    more widget building

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:georgiaonmydime/data/HappyHour.dart';
 import 'package:georgiaonmydime/data/ListItem.dart';
 import 'package:georgiaonmydime/data/OpenStatus.dart';
@@ -12,11 +13,11 @@ void main() => runApp(new MyApp(
           ? new HeadingItem("Heading $i")
           : new HappyHourItem(new HappyHour(
               Weekday.monday,
-              "Happy Hour $i",
-              "Description $i",
-              "https://i.imgur.com/TH0noM3.jpg",
-              "Buckhead",
-              OpenStatus.closed,
+              "Torched Hop Brewing Company",
+              "All Day: ​Every Tuesday the kitchen is whipping up 4 dollar sliders. The options include Pork Belly BLT, Super THC, Fried Avocado, and the SMCC Burger.",
+              "https://georgiaonmydime.com/wp-content/uploads/2018/05/Torched-Hop-Brewing-Company-550x420.jpg",
+              "Midtown",
+              OpenStatus.open,
               false)),
     )));
 
@@ -25,9 +26,14 @@ class MyApp extends StatelessWidget {
 
   final List<ListItem> items;
 
+  void _setWindowProperties() {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    _setWindowProperties();
     return new MaterialApp(
       title: 'Georgia On My Dime',
       theme: new ThemeData(
@@ -52,18 +58,35 @@ class MyApp extends StatelessWidget {
               } else if (item is HappyHourItem) {
                 return new Card(
                     elevation: 4.0,
-                    margin: new EdgeInsets.all(16.0),
+                    margin: new EdgeInsets.all(8.0),
                     child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         new Image.network(item.happyHour.imageUrl),
-                        new Text(item.happyHour.title,
-                            style: Theme.of(context).textTheme.title),
-                        new Text(item.happyHour.description,
-                            style: Theme.of(context).textTheme.body1),
-                        new Text(item.happyHour.location,
-                            style: Theme.of(context).textTheme.body1),
-                        new Text(item.happyHour.day.toString(),
-                            style: Theme.of(context).textTheme.body1)
+                        new Container(
+                          margin: new EdgeInsets.only(
+                              top: 12.0, left: 12.0, right: 12.0, bottom: 24.0),
+                          child: new Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              new Text(item.happyHour.title,
+                                  style: Theme.of(context).textTheme.title),
+                              new Container(
+                                  margin: new EdgeInsets.only(top: 12.0),
+                                  child: new Text(item.happyHour.description,
+                                      style: Theme.of(context).textTheme.body1))
+                            ],
+                          ),
+                        ),
+                        new Divider(height: 4.0, color: Colors.grey),
+                        new Container(
+                            margin: new EdgeInsets.all(12.0),
+                            child: new Row(children: <Widget>[
+                              new Expanded(child: new Text(item.happyHour.location,
+                                  style: Theme.of(context).textTheme.body1)),
+                              new Text(item.happyHour.openStatus.toString(),
+                                  style: Theme.of(context).textTheme.body1)
+                            ]))
                       ],
                     ));
               }
