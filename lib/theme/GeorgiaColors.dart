d35d7070cf6e271da577801f6682f71c975d94b3
Joel Anderson <joelbryceanderson@gmail.com>
2018-06-20 08:49:48 -0400

    custom colors

--- lib/theme/GeorgiaColors.dart
+++ lib/theme/GeorgiaColors.dart
@@ -10,15 +10,16 @@ final ThemeData georgiaThemeData = new ThemeData(
 class GeorgiaColors {
   GeorgiaColors._(); // this basically makes it so you can instantiate this class
 
-  static const _ceruleanBluePrimary = 0xFF007BA7;
+  static const _ceruleanBluePrimary = 0xFF1c70b5;
 
   static const MaterialColor ceruleanBlue = const MaterialColor(
     _ceruleanBluePrimary,
     const <int, Color>{
       100:  const Color(0xFF00a5a7),
-      200: const Color(0xFF0097a7),
-      400: const Color(0xFF0089a7),
-      900: const Color(0xFF007BA7),
+      300: const Color(0xFF0097a7),
+      500: const Color(0xFF0089a7),
+      700: const Color(0xFF007BA7),
+      900: const Color(0xFF1c70b5)
     },
   );
 }
