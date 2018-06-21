48a9bcf40d8baea0abaa4d71bdcd6eb4eae2c4cf
Joel Anderson <joelbryceanderson@gmail.com>
2018-06-20 22:29:49 -0400

    Revert "added url, cleanup header"
    This reverts commit 9ae27af64c8bfb4eb55afe0bea4faf6e113a55ae.

--- lib/widgets/navigation/AppBarBottom.dart
+++ lib/widgets/navigation/AppBarBottom.dart
@@ -4,29 +4,21 @@ class AppBarBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-        margin:
-            EdgeInsets.only(left: 88.0, right: 88.0, bottom: 12.0, top: 88.0),
+        margin: EdgeInsets.only(left: 88.0, right: 88.0, bottom: 12.0, top: 88.0),
         height: 80.0,
         child: new Column(
           children: <Widget>[
-            new Text("Welcome, Joel",
-                style: new TextStyle(fontSize: 18.0, color: Colors.white)),
+            new Text("Welcome, Joel", style: new TextStyle(fontSize: 18.0)),
             new Container(
                 margin: EdgeInsets.only(top: 8.0),
-                child: new Text("It's",
+                child: new Text("Today is",
                     style: new TextStyle(color: Colors.white, fontSize: 12.0))),
             new Row(
               children: <Widget>[
-                new Expanded(
-                    child: new Padding(
-                        padding: new EdgeInsets.only(left: 24.0, right: 8.0),
-                        child: new Divider(color: Colors.white))),
+                new Expanded(child: new Divider(color: Colors.white)),
                 new Text("Monday",
                     style: new TextStyle(color: Colors.white, fontSize: 24.0)),
-                new Expanded(
-                    child: new Padding(
-                        padding: new EdgeInsets.only(left: 8.0, right: 24.0),
-                        child: new Divider(color: Colors.white))),
+                new Expanded(child: new Divider(color: Colors.white)),
               ],
             )
           ],
