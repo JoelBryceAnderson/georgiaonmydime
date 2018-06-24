a9b8c0805d63395a88988041a33f46555b2d9d93
Joel Anderson <joelbryceanderson@gmail.com>
2018-06-24 10:45:59 -0400

    made splash stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:georgiaonmydime/theme/GeorgiaColors.dart';
 import 'package:georgiaonmydime/widgets/screens/main/MainPagerScreen.dart';
 
-void main() => runApp(new SplashScreen());
+void main() => runApp(new MainApp());
 
-class SplashScreen extends StatelessWidget {
+class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MaterialPageRoute.debugEnableFadingRoutes = true; // todo REMOVE THIS!!!
     return new MaterialApp(
         title: 'Georgia On My Dime',
         theme: _appTheme(),
-        home: new SplashWidget());
+        home: new SplashScreen());
   }
 
   ThemeData _appTheme() {
@@ -25,26 +23,42 @@ class SplashScreen extends StatelessWidget {
   }
 }
 
-class SplashWidget extends StatelessWidget {
-  void startApplication(BuildContext context) {
-    Navigator
-        .of(context)
-        .pushReplacement(new MaterialPageRoute(builder: (context) => MainPagerScreen()));
-  }
+class SplashScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _SplashScreenState();
+}
 
-  startTime(BuildContext context) async {
-    var _duration = new Duration(milliseconds: 1000);
-    return new Timer(_duration, () => startApplication(context));
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    animationController = new AnimationController(
+        duration: Duration(milliseconds: 700), vsync: this)
+      ..forward()
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          Navigator.of(context).pushReplacement(new MaterialPageRoute(
+              builder: (context) => new MainPagerScreen()));
+        }
+      });
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    startTime(context);
     return new Scaffold(
       backgroundColor: GeorgiaColors.dimeGray,
       body: new Center(
         child: Image.asset("assets/gomd_icon.png", height: 172.0),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
   }
 }
