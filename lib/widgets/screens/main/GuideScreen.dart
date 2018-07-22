db080c134d6f412d4eb031fc3678731f8cfb3a2f
Joel Anderson <joelbryceanderson@gmail.com>
2018-07-22 14:10:12 -0400

    load guides from server

--- lib/widgets/screens/main/GuideScreen.dart
+++ lib/widgets/screens/main/GuideScreen.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:georgiaonmydime/data/ListItem.dart';
 import 'package:georgiaonmydime/data/posts/Guide.dart';
+import 'package:georgiaonmydime/data/remote/Post.dart';
+import 'package:georgiaonmydime/network/APIReceiver.dart';
 import 'package:georgiaonmydime/theme/GeorgiaColors.dart';
 import 'package:georgiaonmydime/widgets/lists/CardList.dart';
 import 'package:georgiaonmydime/widgets/navigation/MenuAction.dart';
+import 'package:http/http.dart' as http;
 
 class GuideScreen extends StatelessWidget {
   const GuideScreen({Key key, this.items}) : super(key: key);
 
   final List<ListItem> items;
 
-  static List<ListItem> _generateMocksList() {
+  List<ListItem> buildList(List<Post> posts) {
     return new List<ListItem>.generate(
-      15,
-      (i) => new GuideItem(new Guide(
-          "Best Bars to Watch Atlanta United Matches",
-          "As we all know, being an Atlanta sports fan can be pretty depressing. After a heartbreaking Super Bowl loss for the Falcons followed by a Georgia Bulldawgs loss in the Nation Championship (played in Atlanta, of course), it seems like the ATL isn’t much of a football town. Lucky for us, there is a third football team that calls Georgia its home.",
-          "https://georgiaonmydime.com/wp-content/uploads/2018/03/atlanta-united-bars-372x240.jpg",
-          "https://georgiaonmydime.com/best-bars-to-watch-atlanta-united-matches/",
-          "April 23, 2018")),
+      posts.length,
+      (i) => new GuideItem(new Guide(posts[i].title, posts[i].description,
+          posts[i].imageUrl, posts[i].articleUrl, posts[i].published)),
+    );
+  }
+
+  FutureBuilder<List<Post>> buildPostsFromServer() {
+    return FutureBuilder<List<Post>>(
+      future: APIReceiver.guides(http.Client()),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) print(snapshot.error);
+
+        return snapshot.hasData
+            ? CardList(items: buildList(snapshot.data))
+            : SliverFillRemaining(
+                child: new Center(child: new CircularProgressIndicator()));
+      },
     );
   }
 
@@ -35,7 +48,7 @@ class GuideScreen extends StatelessWidget {
           backgroundColor: GeorgiaColors.ceruleanBlue,
           title: new Text("Guides",
               style: new TextStyle(color: Colors.white, fontSize: 24.0))),
-      CardList(items: _generateMocksList())
+      buildPostsFromServer()
     ]);
   }
 }
